dd23d739bc9b03ce7f4f2cf75e2bcfb704a4daf1
netrunner <mohamedchibani420@gmail.com>
2023-06-11 20:07:18 +0000

    linear regression from scratch

--- backprop.py
+++ backprop.py
@@ -1,13 +1,13 @@
 import torch
 
 x = torch.tensor(1.0)
+y = torch.tensor(2.0)
 w = torch.tensor(1.0,requires_grad=True)
-b = torch.tensor(2.0)
-
+b = torch.tensor(3.0)
 #forward pass
-y_hat = x*w
+y_hat = x*w+b
 #compute the loss
-loss = (y_hat-b)**2
+loss = (y_hat-y)**2
 
 loss.backward() #backward pass (dloss/dw chain rule) 
 
---
+++ gradient_descent.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+x = np.array([1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0,11.0,12.0,13.0],dtype=np.float32)
+y = np.array([1.5,2.5,3.5,4.5,5.5,6.5,7.5,8.5,9.5,10.5,11.5,12.5,13.5],dtype=np.float32)
+w = 0.0
+
+
+def forward(x):
+    return x*w
+
+def loss(y_hat,y):
+    return ((y_hat-y)**2).mean()
+
+def gradient_backprop(x,y,y_hat):
+    #dl/dw
+    return np.dot(2*x,y_hat-y).mean()
+
+
+iters = 1000
+lr = 0.001
+
+for epoch in range(iters):
+    y_predicted = forward(x)
+    l = loss(y_predicted,y)
+    gradient = gradient_backprop(x,y,y_predicted)
+    w -= lr * gradient
+    print(f'epoch {epoch} ---> weights : {w} | loss : {l}')
+
+predict = forward(7)
+print(predict)
